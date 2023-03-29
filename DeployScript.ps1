a141da310f0f6f3db1e45a80e3cf6b88a3405403
stevericks <sricks67@gmail.com>
2023-03-29 12:37:18 +0100

    Update DeployScript.ps1

--- DeployScript.ps1
+++ DeployScript.ps1
@@ -1,3 +1,12 @@
 Install-WindowsFeature NET-Framework-Features
 Install-WindowsFeature RSAT-SNMP
 Install-WindowsFeature -Name Web-Server -IncludeManagementTools
+
+# Install Applications using Winget
+
+$winget = https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle
+Invoke-WebRequest $winget -OutFile "winget.appxbundle"
+Add-AppxPackage .\winget.appxbundle
+
+# Install Notepad++
+winget install notepadplusplus
