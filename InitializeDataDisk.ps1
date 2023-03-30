e91155eb3a0034b878404851c6be9a5926321afc
stevericks <sricks67@gmail.com>
2023-03-30 14:37:20 +0100

    Update InitializeDataDisk.ps1

--- InitializeDataDisk.ps1
+++ InitializeDataDisk.ps1
@@ -2,3 +2,7 @@ $disk = Get-Disk -Number 2
 Initialize-Disk -InputObject $disk -PartitionStyle MBR
 New-Partition -DiskNumber $disk.Number -UseMaximumSize -DriveLetter E
 Format-Volume -DriveLetter E -FileSystem NTFS -NewFileSystemLabel "Data"
+
+# Add Welcom Share
+New-Item -ItemType Directory -Path "E:\Welcom"
+New-SmbShare -Name "WelcomShare" -Path "E:\Welcom" -ReadAccess "Domain Users" -FullAccess "Domain Admins", "Welcom"
