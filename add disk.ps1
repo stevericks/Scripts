e37e41b04a3816c8044ddc35e624ff055bc7527a
stevericks <sricks67@gmail.com>
2023-03-30 12:24:41 +0100

    Add files via upload

---
+++ add disk.ps1
@@ -0,0 +1,4 @@
+$disk = Get-Disk -Number 1
+Initialize-Disk -InputObject $disk -PartitionStyle MBR
+New-Partition -DiskNumber $disk.Number -UseMaximumSize -DriveLetter E
+Format-Volume -DriveLetter E -FileSystem NTFS -NewFileSystemLabel "Data"
