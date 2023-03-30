a65f54be9e4150133545ca9a43896a66c6663b5e
stevericks <sricks67@gmail.com>
2023-03-30 13:00:41 +0100

    Update InitializeDataDisk.ps1

--- InitializeDataDisk.ps1
+++ InitializeDataDisk.ps1
@@ -1,4 +1,4 @@
-$disk = Get-Disk -Number 1
+$disk = Get-Disk -Number 2
 Initialize-Disk -InputObject $disk -PartitionStyle MBR
 New-Partition -DiskNumber $disk.Number -UseMaximumSize -DriveLetter E
 Format-Volume -DriveLetter E -FileSystem NTFS -NewFileSystemLabel "Data"
