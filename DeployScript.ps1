c081ecedc6ded76ac371f14f75aac55036dc2a74
stevericks <sricks67@gmail.com>
2023-07-05 14:44:58 +0100

    Update DeployScript.ps1

--- DeployScript.ps1
+++ DeployScript.ps1
@@ -9,5 +9,5 @@ New-Partition -DiskNumber $disk.Number -UseMaximumSize -DriveLetter F
 Format-Volume -DriveLetter F -FileSystem NTFS -NewFileSystemLabel "Data"
 
 # Add Welcom Share
-New-Item -ItemType Directory -Path "F:\Welcom"
-New-SmbShare -Name "WelcomShare" -Path "F:\Welcom" -ReadAccess "Domain Users" -FullAccess "Domain Admins", "Welcom"
+# New-Item -ItemType Directory -Path "F:\Welcom"
+# New-SmbShare -Name "WelcomShare" -Path "F:\Welcom" -ReadAccess "Domain Users" -FullAccess "Domain Admins", "Welcom"
